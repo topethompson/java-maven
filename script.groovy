503e4c9e5e918ee54de2fa62328561b7f7d2c104
Temitope Thompson <topethompson2019@gmail.com>
2026-05-28 12:28:26 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh 'docker build -t ttttttt534/jenkins-git:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
         sh 'docker push ttttttt534/jenkins-git:jma-2.0'
     }
 }
